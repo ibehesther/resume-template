bef0ed42d7d5c895925555a493e29f7738da7df3
Ibeh Esther <estheribeh5@gmail.com>
2020-12-21 22:42:06 +0100

    created resume template

---
+++ script.js
@@ -0,0 +1,18 @@
+// id3= document.querySelector('#id3');
+ 
+// var i = 0;
+// (()=>{
+//     id3.addEventListener('drag', setTimeout(drag, 3000));
+// }) ();
+
+// function drag(){
+//     if (i <= 100){
+//         console.log(i);
+//         id3.style.color='red';
+//         i += 10;
+//         id3.style.marginLeft= `${i}%`;
+//         console.log(i);
+//     }else{
+//         id3.style.marginLeft= '100%';
+//     }
+// }
