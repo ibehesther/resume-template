209e0c7a86dc55a4c65bbd8a9ed1f6c35141e89f
Ibeh Esther <estheribeh5@gmail.com>
2021-01-09 20:55:48 +0100

    worked on making website user friendly

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(()=>{
+const progressBar=()=>{
     var progress1= document.getElementById('progress1');
     progress1.addEventListener('click', ()=>{
         var id1= document.getElementById('id1')
@@ -30,4 +30,46 @@
         progress5.style.backgroundImage= `linear-gradient(to right, #F6B618 ${progress5.value}%,  #ffffff33 ${100 - progress5.value}%)`
     })
    
-}) ();
+} 
+// progressBar();
+
+const navIconFocus=()=>{
+    var navIcon2= document.getElementById("nav5");
+    var circle= document.getElementsByClassName('circle');
+    var no = document.getElementsByClassName('no');
+    var personalDetails = document.getElementById('personal-details');
+    var professionalSkills = document.getElementById('professional-skills');
+    var educationInfo = document.getElementById('education-info');
+    console.log('working');
+    // document.getElementsByTagName('body')[0].onscroll= () => {
+    //     console.log(educationInfo.offsetTop);
+    // };
+    var observer = new IntersectionObserver(function(entries){
+        for (let i=0; i < entries.length; i++){
+            if ( entries[i].intersectionRatio > 0.3  ){
+                console.log(entries.length);
+                console.log(entries);
+                circle[i].style.backgroundColor= '#F6B618';
+                circle[i].style.fontSize= '2vw';
+                no[i].style.visibility='visible';
+                var removeCircle= [...circle];
+                removeCircle.splice(i, 1);
+                var removeNo= [...no];
+                removeNo.splice(i, 1);
+                for (var j=0; j < removeCircle.length; j++){
+                    removeCircle[i].style.backgroundColor= 'rgb(88,88,88)';
+                    removeNo[i].style.visibility='hidden';
+                }
+            }
+        }
+    }, {threshold: [0.1] });
+        observer.observe(personalDetails);
+        observer.observe(educationInfo);
+        observer.observe(professionalSkills);
+   
+   
+    
+    
+    
+}
+navIconFocus();
