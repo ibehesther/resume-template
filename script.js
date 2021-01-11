503465a427d8f6f0a7698bc6e5e05c3bceadcaf7
Ibeh Esther <estheribeh5@gmail.com>
2021-01-11 15:41:41 +0100

    aligned some elements

--- script.js
+++ script.js
@@ -47,19 +47,12 @@ const navIconFocus=()=>{
     var observer = new IntersectionObserver(function(entries){
         for (let i=0; i < entries.length; i++){
             if ( entries[i].intersectionRatio > 0.3  ){
+                // document.body.onload();
                 console.log(entries.length);
                 console.log(entries);
                 circle[i].style.backgroundColor= '#F6B618';
                 circle[i].style.fontSize= '2vw';
                 no[i].style.visibility='visible';
-                var removeCircle= [...circle];
-                removeCircle.splice(i, 1);
-                var removeNo= [...no];
-                removeNo.splice(i, 1);
-                for (var j=0; j < removeCircle.length; j++){
-                    removeCircle[i].style.backgroundColor= 'rgb(88,88,88)';
-                    removeNo[i].style.visibility='hidden';
-                }
             }
         }
     }, {threshold: [0.1] });
